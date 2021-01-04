20b72b6af7f2f884bf744f3d797d9052ca181b0e
Maco <hellqvistmarcus@gmail.com>
2021-01-05 00:08:54 +0100

    removed token

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ async def flush(ctx):
     await ctx.send('Queue cleared.')
     listOfPlayers.clear()
 
-client.run('Nzk0OTAyMDQ4NTQ0OTE1NDc2.X_BkOA.WVTMhYqKHIlj5RwjPcrLQzQrXmE')
+client.run('TOKEN HERE')
